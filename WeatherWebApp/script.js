3d876bbb45f9e2d3a99847b1459e1b1040fd0ed4
Aiturgan <aiturgan888@gmail.com>
2023-11-27 22:14:23 -0600

    js logic

--- WeatherWebApp/script.js
+++ WeatherWebApp/script.js
@@ -1,28 +1,53 @@
-// find the way to find city fetching
-// # access key: 76lSVJPvbAXlVHOUl9Pnn4TQmrMWtjqXknZPgpAUEUI
-
-// # secret key: Ed4xGpiKUXC9b_OkWk8oU2o0Oh9kFnlhcUUMo8nRYTY
-
 const form = document.getElementById('search-form');
 const cityInput = document.getElementById('city-input');
-const cityNameUI = document.getElementsByClassName('location');
-const tempreature = document.getElementsByClassName('temp');
-
+const cityNameUI = document.querySelector('.location');
+const tempreature = document.querySelector('.temp');
+const feelsLike = document.querySelector('.feels .bold');
+const humidity = document.querySelector('.humidity .bold');
+const wind = document.querySelector('.wind .bold');
+const celciusBtn = document.querySelector('.celcius');
+const farenheitBtn = document.querySelector('.farenheit');
+const body = document.querySelector('body');
 let cityName = '';
+let unit = 'imperial';
 
 const getCityTemp = async (cityName) => {
-	const res = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=45b6d1548d52a466627f72a82cb325e5&units=imperial`);
+	const res = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=45b6d1548d52a466627f72a82cb325e5&units=${unit}`);
 	const data = await res.json();
-	console.log(data.name);
 	renderElements(data);
+};
+
+const getCityPic = async (cityName) => {
+	const res = await fetch(`https://api.unsplash.com/search/photos/?client_id=76lSVJPvbAXlVHOUl9Pnn4TQmrMWtjqXknZPgpAUEUI&query=${cityName}`);
+	const data = await res.json();
+	const url = data.results[1].urls.regular;
+	body.style.backgroundImage = `url(${url})`;
 };
 
 form.addEventListener('submit', (event) => {
 	event.preventDefault();
 	cityName = cityInput.value;
 	getCityTemp(cityName);
+	getCityPic(cityName);
+});
+
+celciusBtn.addEventListener('click', () => {
+	farenheitBtn.classList.remove('active');
+	celciusBtn.classList.add('active');
+	unit = 'metric';
+});
+farenheitBtn.addEventListener('click', () => {
+	celciusBtn.classList.remove('active');
+	farenheitBtn.classList.add('active');
+	unit = 'imperial';
 });
 
 const renderElements = (data) => {
+	let tempValue = unit === 'imperial' ? 'F' : 'C';
+	let speedValue = unit === 'imperial' ? 'miles/hr' : 'km/hr';
 	cityNameUI.innerHTML = data.name;
+	tempreature.innerHTML = `${Math.round(data.main.temp)}° <span class="temp-unit">${tempValue}</span>`;
+	feelsLike.innerText = `${Math.round(data.main.feels_like)}°`;
+	humidity.innerText = `${data.main.humidity}%`;
+	wind.innerText = `${data.wind.speed} ${speedValue}`;
 };
